4dc29b2e075064e340eee949ec87b4b10770e29a
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-08-10 12:07:14 +0530

    initial commit

---
+++ src/Core/types.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <string_view>
+#include <type_traits>
+
+namespace exchange::core 
+{
+    using Price = std::int64_t;
+}
